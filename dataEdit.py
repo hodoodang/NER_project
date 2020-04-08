55df4bbb349e3feb28e4a65e4c3f5f47752b94ab
hodoodang <xogh4096@gmail.com>
2020-04-08 17:49:29 +0900

    commit

---
+++ dataEdit.py
@@ -0,0 +1,55 @@
+import json
+import re
+
+# 데이터의 형식을 바꾸는 코드
+# 문장 중 <단어:태그> 형태의 데이터를
+# 번호    단어  태그
+# 번호    단어  태그
+# 형태의 데이터로 바꾼다.
+
+re_word = re.compile('<(.+?):[A-Z]{2}>')
+
+with open('originData/EXOBRAIN_NE_CORPUS_10000.txt', 'r', encoding='utf-8-sig') as of:
+    with open('editData/EXO.txt', 'w', encoding='utf-8-sig') as ef:
+        lines = of.readlines() # 한 문장씩 리스트형으로 받기
+
+        for line in lines:
+            words = re.sub(':[A-Z]{2}', '', line).replace('\ufeff', '').replace('<', ' ').replace('>', ' ').split() # 띄어쓰기 단위로 분리(하나의 어절 안에 두개 이상의 tag가 있는 경우를 위해 '>'를 기준으로 띄어쓰기)
+
+
+            re_result = re_word.finditer(line)
+            temp_list = [] # 태깅된 단어 리스트
+            ner_list = [] # 태깅된 태그 리스트
+
+            for re_item in re_result:
+                # print(re_item.group())
+                re_item_list = re_item.group().replace('<', '').replace('>', '').split(':')
+                temp_list.append(re_item_list[0].split())
+                ner_list.append(re_item_list[-1])
+            #     print(re_item_list)
+            # print(temp_list, ner_list)
+            # print(words)
+            flag = 0
+            j = 0
+            k = 0
+            while j != len(words):
+                if flag == 0 and words[j] in temp_list[k]:
+                    for i_ in range(len(temp_list[k])):
+                        if i_ == 0:
+                            # print(j, len(words), '|', len(temp_list), k)
+                            ef.write(words[j] + '\t' + ner_list[k] + '_B\n')
+                            # print(words[j] + '\t' + ner_list[k] + '_B\n')
+                            j += 1
+                        else:
+                            # print(j, len(words), '|', len(temp_list), k)
+                            ef.write(words[j] + '\t' + ner_list[k] + '_I\n')
+                            # print(words[j] + '\t' + ner_list[k] + '_I\n')
+                            j += 1
+                    k += 1
+                    if k == len(temp_list):
+                        flag = 1
+                else:
+                    ef.write(words[j] + '\t-\n')
+                    # print(words[j] + '\t-\n')
+                    j += 1
+            ef.write('\n')
